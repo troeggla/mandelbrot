de5e458c52a915195412989a327dbb8024a632a9
Thomas Röggla <tom@lichtfragmente.com>
2022-06-12 00:13:14 +0200

    parsing arguments with the crate clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod mandelbrot;
 mod util;
 
-use argparse::{ArgumentParser, Store, StoreTrue};
-use mandelbrot::{get_mandelbrot_color, MandelbrotPoint};
+use clap::Parser;
 use num::complex::Complex;
 use pbr::ProgressBar;
 use std::path::Path;
@@ -10,57 +9,51 @@ use std::sync::mpsc::channel;
 use threadpool::ThreadPool;
 use time::Instant;
 
+use mandelbrot::{get_mandelbrot_color, MandelbrotPoint};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long, help="Print verbose output")]
+    verbose: bool,
+
+    #[clap(long, help="Generate color image")]
+    color: bool,
+
+    #[clap(short, long, default_value="-0.75,0.3", help="Center point of the set to examine")]
+    center: String,
+
+    #[clap(short, long, default_value="1000x1000", help="Dimenions of the output image")]
+    dimensions: String,
+
+    #[clap(short, long, default_value_t=32, help="Number of threads to use")]
+    iterations: u32,
+
+    #[clap(short, long, default_value_t=10, help="Number of threads to use")]
+    threads: usize,
+
+    #[clap(short, long, default_value_t=0.5, help="The radius to examine")]
+    radius: f32,
+
+    #[clap(default_value="fractal.png", help="Output file name")]
+    name: String
+}
+
 fn main() {
-    let mut verbose = false;
-    let mut color = false;
-    let mut dimensions = "1000x1000".to_string();
-    let mut iterations = 250;
-    let mut num_threads = 10;
-    let mut center = "-0.75,0.3".to_string();
-    let mut r: f32 = 0.5;
-    let mut fname = "fractal.png".to_string();
+    let args = Args::parse();
 
-    {
-        let help_texts = vec![
-            format!("Enable verbose output"),
-            format!("Generate output image in colour"),
-            format!("Output image dimensions, separated by 'x' (default {})", dimensions),
-            format!("Centre point of the set separated by comma (default {})", center),
-            format!("Radius of the set to be examined (default {})", r),
-            format!("Number of iterations (default {})", iterations),
-            format!("Number of threads to spawn (default {})", num_threads),
-            format!("Output file name (default '{}')", fname)
-        ];
-
-        let mut ap = ArgumentParser::new();
-
-        ap.set_description("Renders images of portions of the Mandelbrot set.");
-
-        ap.refer(&mut verbose).add_option(&["-v", "--verbose"], StoreTrue, &help_texts[0]);
-        ap.refer(&mut color).add_option(&["--color"], StoreTrue, &help_texts[1]);
-        ap.refer(&mut dimensions).add_option(&["-s", "--size"], Store, &help_texts[2]);
-        ap.refer(&mut center).add_option(&["-c", "--center"], Store, &help_texts[3]);
-        ap.refer(&mut r).add_option(&["-r", "--radius"], Store, &help_texts[4]);
-        ap.refer(&mut iterations).add_option(&["-i", "--iterations"], Store, &help_texts[5]);
-        ap.refer(&mut num_threads).add_option(&["-t", "--threads"], Store, &help_texts[6]);
-        ap.refer(&mut fname).add_option(&["-f", "--fname"], Store, &help_texts[7]);
-
-        ap.parse_args_or_exit();
-    }
-
-    let (width, height) = util::parse_list(dimensions, "x");
-    let center: (f32, f32) = util::parse_list(center, ",");
+    let (width, height) = util::parse_list(args.dimensions, "x");
+    let center: (f32, f32) = util::parse_list(args.center, ",");
 
     let start = Instant::now();
-    let pool = ThreadPool::new(num_threads);
+    let pool = ThreadPool::new(args.threads);
     let (tx, rx) = channel();
 
     let mut imgbuf = image::ImageBuffer::new(width, height);
 
-    if verbose {
+    if args.verbose {
         println!(
             "=> Generating output image of size {}x{} at point ({}, {}) with radius {} and iteration depth {}...",
-            width, height, center.0, center.1, r, iterations
+            width, height, center.0, center.1, args.radius, args.iterations
         );
     }
 
@@ -70,13 +63,13 @@ fn main() {
 
             pool.execute(move|| {
                 let c = Complex::new(
-                    ((x as f32 * r / width as f32) - r / 2.0) + center.0,
-                    -((y as f32 * r / height as f32) - r / 2.0) + center.1
+                    ((x as f32 * args.radius / width as f32) - args.radius / 2.0) + center.0,
+                    -((y as f32 * args.radius / height as f32) - args.radius / 2.0) + center.1
                 );
 
                 let point = MandelbrotPoint{
                     x: x, y: y,
-                    color: get_mandelbrot_color(c, iterations, color)
+                    color: get_mandelbrot_color(c, args.iterations, args.color)
                 };
 
                 tx.send(point)
@@ -93,21 +86,21 @@ fn main() {
             imgbuf.put_pixel(point.x, point.y, point.color);
         }
 
-        if verbose && count % 10000 == 0 {
+        if args.verbose && count % 10000 == 0 {
             progress.add(10000);
         }
         count += 1;
     });
 
-    if verbose {
+    if args.verbose {
         progress.finish();
         println!("=> Saving output image...");
     }
 
-    let _ = image::DynamicImage::ImageRgb8(imgbuf).save(Path::new(&fname));
+    let _ = image::DynamicImage::ImageRgb8(imgbuf).save(Path::new(&args.name));
 
-    if verbose {
-        println!("=> Output image saved as '{}'", fname);
+    if args.verbose {
+        println!("=> Output image saved as '{}'", args.name);
         println!("=> Time taken: {:.2}s", start.elapsed().as_seconds_f64());
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,3 @@
-use std;
-
 pub fn parse_list<T: std::str::FromStr>(dimensions: String, delimiter: &str) -> (T, T) {
     let mut result: Vec<T> = dimensions.split(delimiter).take(2).map(|s| {
         s.parse::<T>().ok().unwrap()
